e285536a9d0799f23a54fd92391f925b09d431a9
Emiliia <emiliatoropova@yandex.ru>
2023-04-06 21:57:45 +0300

    init m1-t1

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Index></Index>
+  </div>
+</template>
+
+<script>
+  import Index from '@/views/Index.vue';
+
+  export default {
+    name: 'App',
+    components: {Index},
+  };
+</script>
+
+<style lang="scss">
+  @import "~@/assets/scss/app.scss";
+</style>
---
+++ src/frontend/src/common/const/constants.js
@@ -0,0 +1,37 @@
+export const DOUGHT = {
+  Тонкое: {
+    value: 'light',
+  },
+  Толстое: {
+    value: 'large',
+  },
+};
+
+export const SIZE = {
+  '23 см': 'small',
+  '32 см': 'normal',
+  '45 см': 'big',
+};
+
+export const SAUCE = {
+  Томатный: 'tomato',
+  Сливочный: 'creamy',
+};
+
+export const INGREDIENTS = {
+  Ананас: 'ananas',
+  Бекон: 'bacon',
+  'Блю чиз': 'blue_cheese',
+  Ветчина: 'ham',
+  Грибы: 'mushrooms',
+  Лосось: 'salmon',
+  Лук: 'onion',
+  Маслины: 'olives',
+  Моцарелла: 'mozzarella',
+  Пармезан: 'parmesan',
+  Салями: 'salami',
+  Томаты: 'tomatoes',
+  Халапеньо: 'jalapeno',
+  Чеддер: 'cheddar',
+  Чили: 'chile',
+};
